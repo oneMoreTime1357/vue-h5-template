5e622052f2c5257a02962abf667571754e8d35e3
yangdan <1345335516@qq.com>
2020-09-19 23:02:32 +0800

    Updates

---
+++ precache-manifest.e54ef9db7a9ab9389b941678c9a02cd7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ca59ef4cbe743137418",
+    "url": "/css/app.df19830d.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "25482a52d2459f3c83f58f8d322b1d02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "943291f0b1149c52d934",
+    "url": "/js/about.e4d6296e.js"
+  },
+  {
+    "revision": "5ca59ef4cbe743137418",
+    "url": "/js/app.069a7da4.js"
+  },
+  {
+    "revision": "7522c0720069a6f47515",
+    "url": "/js/chunk-vendors.ce061d87.js"
+  },
+  {
+    "revision": "c6ff614eb269cb442d74817f154050c8",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
