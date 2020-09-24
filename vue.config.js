babe8fae47eafab27d5e14d75f458dc4d396c40e
yangdan <1345335516@qq.com>
2020-09-24 21:36:45 +0800

    feat: add gh-pages

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue-h5-template/' : '/',
   // css: {
   //   loaderOptions: {
   //     sass: {
